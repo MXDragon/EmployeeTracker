cdac9e558a18dad08c3c9bd1fa9b462543a2e10a
hurondragon <bentzbry@gmail.com>
2021-06-24 12:00:56 -0400

    Adding department seeds

--- schema.sql
+++ schema.sql
@@ -32,3 +32,13 @@ CREATE TABLE employee (
   FOREIGN KEY (manager_id) REFERENCES employee(id)
 
 );
+
+-- DEPARTMENT SEEDS -----
+INSERT INTO department (name)
+VALUE ("Sales");
+INSERT INTO department (name)
+VALUE ("Engineering");
+INSERT INTO department (name)
+VALUE ("Finance");
+INSERT INTO department (name)
+VALUE ("Legal");
