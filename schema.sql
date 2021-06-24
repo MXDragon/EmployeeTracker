e9d5561303f1762d22c27dc29f773aef45548baf
hurondragon <bentzbry@gmail.com>
2021-06-24 12:00:17 -0400

    Creating department tables

--- schema.sql
+++ schema.sql
@@ -5,3 +5,18 @@ DROP DATABASE IF EXISTS employee_trackerDB;
 CREATE DATABASE employee_trackerDB;
 
 USE employee_trackerDB;
+
+-- DEPARTMENT TABLE ----
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+ 
+);
+-- DEPARTMENT TABLE ----
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
