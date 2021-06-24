b9c6c01e93b88bdc41b0df07e11f7d632fdf3cf1
hurondragon <bentzbry@gmail.com>
2021-06-24 11:50:27 -0400

    Working on app.js viewAllDepartments

--- app.js
+++ app.js
@@ -88,4 +88,12 @@ function viewAllRoles() {
     })
   }
 
-  
+//============= View All Employees By Departments ==========================//
+function viewAllDepartments() {
+    connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;", 
+    function(err, res) {
+      if (err) throw err
+      console.table(res)
+      startPrompt()
+    })
+  }
